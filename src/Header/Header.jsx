365044317f6c1e9ded60692728bacc863c8e5a9f
GlebShevardin <shevardingleb10@gmail.com>
2024-06-18 01:43:30 +0300

    Code optimization

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,22 +2,31 @@ import c from './Header.module.css'
 
 const Header = () => {
     return (
-        <>
-            <div className={c.content}>
-                <div className={c.container}>
-                    <div className={c.title}>
-                        Search for Books
-                    </div>
+        <div className={c.background}>
+            <div className={c.container}>
+                <div className={c.title}><a>Search for Books</a></div>
+                <input type="text" className={c.bookNameInput} placeholder="Enter book name" />
+                <div className={c.bookSelect}>
+                    <select name="categories" className={`${c.categorySelect} ${c.select}`}>
+                        <option value="0">Category</option>
+                        <option value="1">Первый вариант</option>
+                        <option value="2">Второй вариант</option>
+                        <option value="3">Третий вариант</option>
+                        <option value="4">Четвертый вариант</option>
+                        <option value="5">Пятый вариант</option>
+                    </select>
 
-                    <input type="text" className={c.bookTitleInput} placeholder="Enter book name" />
-
-                    <div className={c.bookCategories}>
-                        <input type="text" className={c.bookCategoriesInput} placeholder="Categories" />
-                        <input type="text" className={c.bookSortInput} placeholder="Sort" />
-                    </div>
+                    <select name="sort" className={`${c.sortSelect} ${c.select}`}>
+                        <option value="0">Sort by</option>
+                        <option value="1">Первый вариант</option>
+                        <option value="2">Второй вариант</option>
+                        <option value="3">Третий вариант</option>
+                        <option value="4">Четвертый вариант</option>
+                        <option value="5">Пятый вариант</option>
+                    </select>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
