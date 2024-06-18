dfe0e4f9c04a7c4ada056353833f7795f147ee98
GlebShevardin <shevardingleb10@gmail.com>
2024-06-18 14:41:24 +0300

    Added tips for  inputs

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -6,9 +6,16 @@ const Header = () => {
             <div className={c.container}>
                 <div className={c.title}><a>Search for Books</a></div>
                 <input type="text" className={c.bookNameInput} placeholder="Enter book name" />
+
                 <div className={c.bookSelect}>
+
+                    <div className={c.tips}>
+                        <a className={c.categoryTip}>categories</a>
+                        <a className={c.sortTip}>sort by</a>
+                    </div>
+
                     <select name="categories" className={`${c.categorySelect} ${c.select}`}>
-                        <option value="0">Category</option>
+                        <option value="0">All</option>
                         <option value="1">Первый вариант</option>
                         <option value="2">Второй вариант</option>
                         <option value="3">Третий вариант</option>
@@ -17,7 +24,7 @@ const Header = () => {
                     </select>
 
                     <select name="sort" className={`${c.sortSelect} ${c.select}`}>
-                        <option value="0">Sort by</option>
+                        <option value="0">Relevance</option>
                         <option value="1">Первый вариант</option>
                         <option value="2">Второй вариант</option>
                         <option value="3">Третий вариант</option>
