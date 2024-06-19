3a79e5f37934ba08ce171dce648c8a2df781ca9e
GlebShevardin <shevardingleb10@gmail.com>
2024-06-19 13:17:00 +0300

    Body section laid out

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,10 +1,33 @@
 import c from './Body.module.css'
+import BookCard from './bookInforamationCard/BookCard'
 
 const Body = () => {
     return (
         <>
-        <div className={c.content}>
-            Body content
+        <div className={c.container}>
+            <div className={c.foundResults}>
+                <a>Found 446 results</a>
+                <div className={c.line}/>
+            </div>
+
+            <div className={c.cards}>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+            </div>
         </div>
         </>
     )
